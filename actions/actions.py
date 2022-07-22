a14ac6c72dc2759b25b6d82656fb67ec96a10a10
pranav6226 <pramavmm25@gmail.com>
2022-07-22 12:07:36 +0530

    Added values to details

--- actions/actions.py
+++ actions/actions.py
@@ -12,24 +12,63 @@ class ValidateDetailsForm(FormValidationAction):
     def name(self) -> Text:
         return "validate_details_form"
 
-    def validate_name(
+    def validate_fname(
         self,
         slot_value: Any,
         dispatcher: CollectingDispatcher,
         tracker: Tracker,
         domain: DomainDict,
     ) -> Dict[Text, Any]:
-        """Validate `name` value."""
+        """Validate `fname` value."""
 
         # Matching RegEx for Name.
         pattern = "[a-zA-Z][a-zA-Z ]+[a-zA-Z]$"
         test_string = slot_value
         result = re.match(pattern, test_string)
         if result:
-            return {"name": slot_value}
+            return {"fname": slot_value}
         else:
             dispatcher.utter_message(text="Please enter a valid Name")
-            return {"name": None}
+            return {"fname": None}
+
+    def validate_lname(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `lname` value."""
+
+        # Matching RegEx for Name.
+        pattern = "[a-zA-Z][a-zA-Z ]+[a-zA-Z]$"
+        test_string = slot_value
+        result = re.match(pattern, test_string)
+        if result:
+            return {"lname": slot_value}
+        else:
+            dispatcher.utter_message(text="Please enter a valid Name")
+            return {"lname": None}
+
+    def validate_company(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `company` value."""
+
+        # Matching RegEx for Name.
+        pattern = "/^[.@&]?[a-zA-Z0-9 ]+[ !.@&()]?[ a-zA-Z0-9!()]+/"
+        test_string = slot_value
+        result = re.match(pattern, test_string)
+        if result:
+            return {"company": slot_value}
+        else:
+            dispatcher.utter_message(text="Please enter a valid Company Name")
+            return {"Company": None}
+
 
     def validate_phone(
         self,
@@ -88,11 +127,15 @@ class ActionSaveConversation(Action):
                 chat_data+=f"bot: {i['text']}\n"
         client = MongoClient("mongodb+srv://pranavmm:IKJmGfPCmq2OeYpG@cluster0.ywa3s.mongodb.net/?retryWrites=true&w=majority")
         db=client.get_database('Chatbot')
-        name = tracker.get_slot("name")
+        fname = tracker.get_slot("fname")
+        lname = tracker.get_slot("lname")
+        company = tracker.get_slot("company")
         phone = tracker.get_slot("phone")
         email = tracker.get_slot("email")
         details = {
-                'name' : name,
+                'First Name' : fname,
+                'Last Name' : lname,
+                'Company Name': company,
                 'phone' : phone,
                 'email' : email,
                 'transcript': chat_data
